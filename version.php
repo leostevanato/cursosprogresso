277ec1b18a5dfa4d408cea35ebdc3545cc771bc8
leostevanato <47180899+leostevanato@users.noreply.github.com>
2024-03-18 14:25:43 -0300

    Atualizar alinhamento da lista para o Moodle 3.6

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_cursosprogresso';
 $plugin->release = '0.1.0';
-$plugin->version = 2024031801;
+$plugin->version = 2024031803;
 $plugin->requires = 2018120300;
 $plugin->maturity = MATURITY_BETA;
